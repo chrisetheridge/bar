b26b44fe9688130661e52a28d26b938ae5dbe20e
chrisetheridge <chris.etheridge@cognician.com>
2016-01-26 09:05:43 +0200

    Updated some commands

--- commands/update.js
+++ commands/update.js
@@ -16,67 +16,67 @@ var getActiveWindow = function(){
   return awPromise;
 };
 
+
 // Check soundcloud file for currenty playing
 var browserPlaying = function(){
-  stat = fs.statSync("bar/playing/browser");
-  lastModified = new Date(stat.mtime);
-  data = fs.readFileSync("bar/playing/browser", "utf-8");
+    stat = fs.statSync("bar/playing/browser");
+    lastModified = new Date(stat.mtime);
+    data = fs.readFileSync("bar/playing/browser", "utf-8");
 
-  if(!!data && new Date().getTime() - lastModified.getTime() < 5000){
-    data = JSON.parse(data)
-  }
+    if(!!data && new Date().getTime() - lastModified.getTime() < 5000){
+        data = JSON.parse(data)
+    }
 
-  return data;
+    return data;
 };
 
 var spotifyPlaying = function(){
-  var promise = new Promise();
-  var state = {
-    track: "",
-    playing: false,
-    source: "spotify"
-  };
+    var promise = new Promise();
+    var state = {
+        track: "",
+        playing: false,
+        source: "spotify"
+    };
 
-  spotify.getState(function(err, s){
-    if (!err && !!s){
-      state.playing = s.state == "playing";
-      if(state.playing){
-        spotify.getTrack(function(err, track){
-          if(!err && !!track && state.playing){
-            state.track = track.artist + ' - ' + track.name;
-          }
-          promise.resolve(state);
-        });
-      } else {
-        promise.resolve(state);
-      }
-    } else {
-      promise.resolve(state);
-    }
-  });
+    spotify.getState(function(err, s){
+        if (!err && !!s){
+            state.playing = s.state == "playing";
+            if(state.playing){
+                spotify.getTrack(function(err, track){
+                    if(!err && !!track && state.playing){
+                        state.track = track.artist + ' - ' + track.name;
+                    }
+                    promise.resolve(state);
+                });
+            } else {
+                promise.resolve(state);
+            }
+        } else {
+            promise.resolve(state);
+        }
+    });
 
-  return promise;
+    return promise;
 };
 
 var playing = {
-  browser: browserPlaying(),
-  date: strftime("%a %d %b"),
-  time: strftime("%l:%M"),
-  active: getActiveWindow()
+    browser: browserPlaying(),
+    date: strftime("%a %d %b"),
+    time: strftime("%l:%M"),
+    active: getActiveWindow()
 };
 
 var promises = [
-  // Update battery level
-  batteryLevel(),
-  spotifyPlaying(),
-  getActiveWindow(),
+    // Update battery level
+    batteryLevel(),
+    spotifyPlaying(),
+    getActiveWindow(),
 ];
 
 var p = all(promises)
 p.then(function(data){
-  playing.battery = data[0] * 100;
-  playing.spotify = data[1];
-  playing.active = data[2];
-  console.log(JSON.stringify(playing));
+    playing.battery = data[0] * 100;
+    playing.spotify = data[1];
+    playing.active = data[2];
+    console.log(JSON.stringify(playing));
 });
-
